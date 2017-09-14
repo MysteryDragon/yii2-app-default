83cb813b1604681c07dd3fdc97cf35ec219e4462
Pavel Dovlatov <mysterydragon@yandex.ru>
2017-09-14 15:02:09 +0300

    Configuration bug fix

--- environments/dev/config/main-local.php
+++ environments/dev/config/main-local.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = [
+return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
@@ -18,18 +18,3 @@ $config = [
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
-
-return $config;
